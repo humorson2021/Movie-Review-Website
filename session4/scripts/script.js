148afa6fceccfab5ae2baa51f965492dbd8d53de
yuze <humourson@gmail.com>
2022-05-23 00:24:39 -0700

    function practice

--- session4/scripts/script.js
+++ session4/scripts/script.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= 10; i++) {
 let j = 1;
 while (j <= 10) {
     console.log(Math.pow(2, j));
-    j++
+    j++;
 }
 
 let user;
@@ -15,6 +15,17 @@ do{
 }while(user.length === 1 || !isNaN(user))
 
 alert(`welcome ${user}!`);
+
+
+function totalprice(bill, taxrate = 0.12, tiprate = 0.15){
+    return bill * (1 + taxrate + tiprate);
+}
+
+function printprice(func, billamount){
+    console.log(func(billamount));
+}
+
+printprice(totalprice, 100);
 
 let car = {
     make:"mazda",
