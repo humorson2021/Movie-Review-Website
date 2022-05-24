7d2d21d693625897afa205eb8fee0d3836983bec
yuze <humourson@gmail.com>
2022-05-24 15:10:57 -0700

    not working of green item

--- week3/session5/scripts/main.js
+++ week3/session5/scripts/main.js
@@ -1,19 +1,23 @@
-/* function getNumber() {
-    let num;
-    do {
-        num = prompt("enter a number");
-    } while (isNaN(num));
-    return num;
-}
+// function getNumber() {
+//     let num;
+//     do {
+//         num = prompt("enter a number");
+//     } while (isNaN(num));
+//     return num;
+// }
 
-let number = getNumber();
+// let number = getNumber();
 let radius = document.querySelector("#radius");
-radius.innerText += number;
+radius.classList.add('paragraph');
+//radius.setAttribute("class", "paragraph");
+//radius.style = "color:red";
 
-function areaOfCircle(radius) {
-    let area = Math.PI * Math.pow(radius, 2);
-    return area;
-} */
+// radius.innerText += number;
+
+// function areaOfCircle(radius) {
+//     let area = Math.PI * Math.pow(radius, 2);
+//     return area;
+// }
 
 let shoppingItems = ['bread', 'cheese', 'green pepper'];
 let list = document.querySelector('.shopping');
@@ -29,3 +33,21 @@ function updateShoppingList(items)
 }
 
 updateShoppingList(shoppingItems);
+
+function squareList(list) {
+    for (let item of list) {
+        item = Math.pow(item, 2);
+    }
+}
+
+function greenItem() {
+    let listItems = document.querySelectorAll('.shopping li');
+    for (let item of listItems) {
+        if (item.includes("green")) {
+            item.classList.add('greenItem');
+        }
+    }
+    //console.log(listItems);
+}
+
+greenItem();
