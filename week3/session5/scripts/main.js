3fd77f1e07e409b03b11138c602456d635547f18
yuze <humourson@gmail.com>
2022-05-24 14:42:23 -0700

    shopping items

--- week3/session5/scripts/main.js
+++ week3/session5/scripts/main.js
@@ -1,4 +1,4 @@
-function getNumber() {
+/* function getNumber() {
     let num;
     do {
         num = prompt("enter a number");
@@ -11,6 +11,21 @@ let radius = document.querySelector("#radius");
 radius.innerText += number;
 
 function areaOfCircle(radius) {
-    let area = Math.PI * radius * radius;
+    let area = Math.PI * Math.pow(radius, 2);
     return area;
+} */
+
+let shoppingItems = ['bread', 'cheese', 'green pepper'];
+let list = document.querySelector('.shopping');
+function updateShoppingList(items) 
+{
+    for (let item of items)
+    {
+        let listItem = document.createElement("li");
+        listItem.textContent = item;
+        list.append(listItem);
+        //list.append(item + " ");
+    }
 }
+
+updateShoppingList(shoppingItems);
