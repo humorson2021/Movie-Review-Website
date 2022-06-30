c3b30890628cc3c25d97dd1b2e7a53623baed7cb
yuze <humourson@gmail.com>
2022-06-30 00:51:51 -0700

    more tests

--- FinalProject/client/src/components/Movie.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Task from './Movie';
-import {BrowserRouter} from 'react-router-dom';
-
-test("searched movie is not shown, keep loading", async () => {
-    render(<Task/>, {wrapper: BrowserRouter});
-    const divArray = screen.getAllByRole("div");
-    expect(divArray).toHaveLength(1);
-})
--- FinalProject/client/src/components/MovieDetails.test.js
+++ FinalProject/client/src/components/MovieDetails.test.js
@@ -28,3 +28,20 @@ test("callback is being called", async () => {
     expect(callBackFunction).toHaveBeenCalledTimes(0);
 
 });
+
+
+// import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { BrowserRouter } from 'react-router-dom';
+// import AddComment from './AddComment';
+// import { Auth0Provider } from "@auth0/auth0-react";
+
+// test("add comment form", async () => {
+//     render(<AddComment />, {wrapper: BrowserRouter, Auth0Provider});
+//     const addButton = screen.getByRole("button", {
+//         name: /Submit your comment/i,
+//     });
+//     expect(addButton).toBeInTheDocument();
+//     await userEvent.click(addButton);
+
+// });
